f4bdfaca9a57344a242157e7548ae0d6cef2fd7f
rawanmahdi <mahdir3@mcmaster.ca>
2023-10-03 16:02:50 -0400

    tiny fix to paths

--- app/src/app.py
+++ app/src/app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 with st.sidebar:
-    st.image("./imgs/McMaster_logo.png")
+    st.image("../../imgs/McMaster_logo.png")
     st.title("SpawnML")
     choice = st.radio("Navigation", ["Upload", "Profiling", "ML", "Download"])
     st.info(
         "SpawnML enables developers with limited machine learning expertise to train high-quality models specific to their business needs. Build your own custom machine learning model in minutes."
     )
-    st.image("./imgs/GDSC_logo.png")
+    st.image("../../imgs/GDSC_logo.png")
 
 
 if choice == "Upload":
