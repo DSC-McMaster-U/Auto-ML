90fc433ba51a61c9130019c88a76ab0d40f9dfc2
LaeekAhmed <laeek385@gmail.com>
2023-10-29 13:47:18 -0400

    Frontend: basic request to API

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/api/python")
+async def root():
+    return {"message": "Hello from fastAPI backend"}
