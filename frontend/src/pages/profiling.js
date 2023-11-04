6624b7d18d9099e545bb9c142748f5269f3dd1c0
bahld1 <144386012+bahld1@users.noreply.github.com>
2023-11-04 12:16:34 -0400

    Update profiling.js

--- frontend/src/pages/profiling.js
+++ frontend/src/pages/profiling.js
@@ -1,1 +1,9 @@
+from operator import index
+import streamlit as st
+import pandas_profiling
+import pandas as pd
+from streamlit_pandas_profiling import st_profile_report
 
+st.title("Exploratory Data Analysis")
+    profile_df = df.profile_report()
+    st_profile_report(profile_df)
