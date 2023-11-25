0358858409ee5840f2da4330bd77a9030a3220f0
Andrew Bovbel <andrew@bovbel.com>
2023-11-24 22:33:47 -0500

    feat: added data upload

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import csv
+from io import StringIO
 
 app = FastAPI()
+
+data = {}
 
 @app.get("/")
 async def root():
@@ -9,3 +13,31 @@ async def root():
 @app.get("/api/python")
 async def root():
     return {"message": "Hello from fastAPI backend"}
+
+@app.get("/api/dataset")
+async def getData():
+    return data
+
+@app.post("/api/dataset")
+def upload(file: UploadFile):  # Initialize data as a dictionary
+
+    if not file:
+        return {"message": "No upload file sent"}
+
+    try:
+        contents = file.file.read()
+
+        with StringIO(contents.decode('utf-8')) as buffer:
+            csvReader = csv.DictReader(buffer)
+
+            for row in csvReader:
+                key = row.get('Id')  # Assuming a column named 'Id' to be the primary key
+                data[key] = row
+
+    except Exception as e:
+        return {"error": f"An error occurred: {str(e)}"}
+
+    finally:
+        file.file.close()
+
+    return data
