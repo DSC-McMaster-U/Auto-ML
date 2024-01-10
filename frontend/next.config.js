a20a0572a3b5d6bfc1e3fe91ebcd182f92adece1
LaeekAhmed <laeek385@gmail.com>
2024-01-09 23:20:05 -0500

    Connected backend & frontend containers both locally and on gcloud

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,24 +4,15 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
+        destination: "http://backend-service:8000/api/:path*"
       },
       {
         source: "/docs",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
+        destination: "http://backend-service:8000/docs"
       },
       {
         source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
+        destination: "http://backend-service:8000/openapi.json"
       },
     ];
   },
