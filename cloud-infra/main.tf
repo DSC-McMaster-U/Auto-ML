5e7d6c19e7c680466ae0967b3961e634a3ad5b16
rawanmahdi <mahdir3@mcmaster.ca>
2024-01-26 19:07:41 -0500

    Changed bucket name to new unique name

--- cloud-infra/main.tf
+++ cloud-infra/main.tf
@@ -67,7 +67,7 @@ resource "google_container_node_pool" "ml_node_pool" {
 // Create new storage bucket in the US multi-region with standard storage
 resource "google_storage_bucket" "static" {
   project       = var.project
-  name          = "data-test-automate-ml"
+  name          = "data-automate-ml"
   location      = "us-east1"
   storage_class = "standard"
 
