68d403c534a5808f2a354758eebf4a42bd7fc93f
rawanmahdi <mahdir3@mcmaster.ca>
2024-04-01 21:13:15 -0400

    fix data profiling page

--- backend/compute/autoEDA.py
+++ backend/compute/autoEDA.py
@@ -2,17 +2,22 @@ import json
 import uuid
 import pandas as pd
 import matplotlib.pyplot as plt
-#from ydata_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 
+# main function to generate the EDA report
+def profile(file_path):
+    df = pd.read_csv(file_path)
+    profile = ProfileReport(df, title="Profiling Report")
+    unique_filename = f"your_report_{uuid.uuid4()}.html"
+    profile.to_file(f"tempHTML/{unique_filename}")
+    return unique_filename
 
+# legacy - not called by backend 
 def generate_eda_plots(df):
     # Create a correlation matrix
     corr_matrix = df.corr()
 
-    # Create variable distributions
-    variable_distributions = sns.pairplot(df)
-
-    return corr_matrix, variable_distributions
+    return corr_matrix
 
 
 def analyze_csv_and_plot(file_path):
@@ -20,7 +25,7 @@ def analyze_csv_and_plot(file_path):
     df = pd.read_csv(file_path)
 
     # Generate EDA plots
-    corr_matrix, variable_distributions = generate_eda_plots(df)
+    corr_matrix = generate_eda_plots(df)
 
     # Display the plots (or save them to files if needed)
     plt.show()
@@ -42,23 +47,8 @@ def generate_eda(file_path):
 
     # Create variable distributions (pairplot) and save it as a PNG
     plt.figure(figsize=(10, 10))
-    variable_distributions = sns.pairplot(df)
 
-    unique_filename = f"variable_distributions_{uuid.uuid4()}.png"
-    variable_distributions.savefig(f"tempImages/{unique_filename}")
-    plt.close()
-
-    return corr_matrix, unique_filename
-
-
-# # Example: Provide the path to your CSV file
-csv_file_path = (
-    r"C:/Users/Rawan Alamily/Downloads/GDSC/Auto-ML/backend/data/electric_cars1.csv"
-)
-
-# analyze_csv_and_plot(csv_file_path)
-
-# generate_eda(r"/Users/abedm/Documents/repos/Auto-ML/backend/compute/data.csv")
+    return corr_matrix
 
 
 def get_nulls(file_path):
@@ -81,19 +71,3 @@ def get_distributions(file_path):
 def get_types(file_path):
     df = pd.read_csv(file_path)
     return df.dtypes
-
-
-def profile(file_path):
-    df = pd.read_csv(file_path)
-    profile = ProfileReport(df, title="Profiling Report")
-    unique_filename = f"your_report_{uuid.uuid4()}.html"
-    profile.to_file(f"tempHTML/{unique_filename}")
-    return unique_filename
-
-
-# print(get_nulls("/Users/abedm/Documents/repos/Auto-ML/backend/compute/data.csv"))
-# print(
-#     get_distributions("/Users/abedm/Documents/repos/Auto-ML/backend/compute/data.csv")
-# )
-# print(get_types("/Users/abedm/Documents/repos/Auto-ML/backend/compute/data.csv"))
-# print(profile("/Users/abedm/Documents/repos/Auto-ML/backend/compute/data.csv"))
